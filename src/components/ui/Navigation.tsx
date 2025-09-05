d07a005e1f62514b9dd34a8ff6388f8c525ef555
gretwix <aleherreravalllejo@gmail.com>
2025-09-04 21:27:31 -0600

    agregar rutas

---
+++ src/components/ui/Navigation.tsx
@@ -0,0 +1,9 @@
+import {Link} from "@tanstack/react-router";
+
+export default function Navigation(){
+    return(
+        <Link
+            to="/"
+            className="text-lime-400">Home</Link>
+    )
+}
---
+++ src/components/ui/pages/adminUsers.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import Header from "../Header";
+
+export default function AdminUsers() {
+
+    return(
+        <div>
+            <Header />
+        </div>
+    )
+
+}
